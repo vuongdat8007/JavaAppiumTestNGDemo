2801f058d066f2c1c1c57662643246a021a0a4d8
vuongdat8007 <vuongdat@gmail.com>
2024-04-23 23:01:20 +0700

    init

---
+++ src/main/java/datvuong/Appium/App.java
@@ -0,0 +1,13 @@
+package datvuong.Appium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
